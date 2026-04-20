294cc33876362b971277827c701aa72ef40d82b7
AneeshPal <2k23.cs2313398@gmail.com>
2026-04-20 23:46:22 +0530

    started working on the homepage layout

---
+++ HyperLocal Commerce/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Nav from "./Nav"
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Nav></Nav>
+             
+    </>
+  )
+}
+
+export default App
---
+++ HyperLocal Commerce/src/Nav.jsx
@@ -0,0 +1,12 @@
+
+function Nav() {
+    return (
+        <>
+            <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+            </nav>
+
+        </>
+    )
+}
+
+export default Nav
